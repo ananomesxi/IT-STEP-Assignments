a9a40c5a73c78978cb65766733c07006fe613823
ananomesxi <mesxi032@gmail.com>
2026-05-28 16:19:46 +0400

    added assignment 7

---
+++ Assignment 7/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7
+{
+    public class Product
+    {
+
+        #region fields
+
+        public int Id;
+        public string Name;
+        public string Description;
+        public double Price;
+        public int Quantity;
+        public string Brand;
+        public string Category;
+        public double Rating;
+        public bool IsAvailable;
+        public double DiscountPercent;
+        public int NumberOfRatings;
+
+        #endregion
+
+
+        #region methods
+
+        public double DiscountedPrice ()
+        {
+            return Price * (100 - DiscountPercent) / 100;
+        }
+
+        public int AddProducts (int added)
+        {
+            return Quantity + added;
+        }
+
+        public void PrintInfo ()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Quantity: {Quantity}");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Category: {Category}");
+            Console.WriteLine($"Rating: {Rating}");
+            Console.WriteLine($"IsAvailable: {IsAvailable}");
+            Console.WriteLine($"DiscountPercent: {DiscountPercent}");
+        }
+
+        public double NewRating (double newRating)
+        {
+            return (newRating + NumberOfRatings * Rating) / (NumberOfRatings + 1);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Assignment 7/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Assignment_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product phone = new Product();
+
+            //კონსტრუქტორით არ ჩავწერე და ჩამონათვალში დავწერე,
+            //რადგან ბევრი ინფოა და დამაბნეველი იყო უსახელოდ ჩაწერა ყველაფრის
+
+            phone.Id = 1015;
+            phone.Name = "Iphone 15";
+            phone.Description = "Black, 6.1-inch, 128GB";
+            phone.Price = 1000;
+            phone.Quantity = 10;
+            phone.Brand = "Apple";
+            phone.Category = "Smartphones";
+            phone.Rating = 4.7;
+            phone.IsAvailable = true;
+            phone.DiscountPercent = 25;
+            phone.NumberOfRatings = 15;
+
+
+            #region method 1
+
+            Console.WriteLine($"Original price is {phone.Price}. The discounted price is {phone.DiscountedPrice()}");
+
+            #endregion
+
+
+            #region method 2
+
+            Console.WriteLine($"Quantity is {phone.Quantity}. How many would you like to add?");
+            bool isValidAdded = int.TryParse(Console.ReadLine(), out int added);
+
+            if (isValidAdded)
+            {
+                Console.WriteLine($"The total number after adding is {phone.AddProducts(added)}.");
+            }
+
+            #endregion
+
+
+            #region method 3
+
+            Console.WriteLine("Would you like to print phone information?\nEnter Yes or No");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes")
+            {
+                phone.PrintInfo();
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Have a nice day!");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+
+            #endregion
+
+
+            #region method 4
+
+            Console.WriteLine("Would you like to add a new rating?\nEnter Yes or No");
+            answer = Console.ReadLine().ToLower();
+            if (answer == "yes")
+            {
+                bool isValidRating = double.TryParse(Console.ReadLine(), out double userRating);
+
+                if (isValidRating)
+                {
+                    Console.WriteLine($"The new rating is: {phone.NewRating(userRating)}");
+                }
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Have a nice day!");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+
+            #endregion
+        }
+    }
+}
