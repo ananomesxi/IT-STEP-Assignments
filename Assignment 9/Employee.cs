512cf78a878af56d834f6adb97a256d8ce572334
ananomesxi <mesxi032@gmail.com>
2026-06-05 16:46:54 +0400

    added assignment 9

---
+++ Assignment 9/Employee.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_9
+{
+    public enum Countries { 
+        Georgia,
+        Greece,
+        Italy,
+        Germany
+    }
+
+    public enum Genders
+    {
+        Male,
+        Female,
+        Unknown
+
+    }
+
+    public enum Contacts
+    {
+        Email,
+        Phone,
+        Fax
+    }
+
+    internal class Employee
+    {
+        private string _name;
+        private string _surname;
+        private DateTime _dateOfBirth;
+
+        public Employee(string name, string surname, DateTime dateOfBirth, Countries country, Genders gender, Contacts contact)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+            Country = country;
+            Gender = gender;
+            Contact = contact;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) // ნალი და ცარიელი სტრინგი არ გვაწყობს
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) // ნალი და ცარიელი სტრინგი არ გვაწყობს
+                {
+                    _surname = value;
+                }
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return _dateOfBirth;
+            }
+            set
+            {
+                if (value <= DateTime.Now.AddYears(-16))  // ლეგალურად მუშაობის ასაკს ამოწმებს
+                {
+                    _dateOfBirth = value;
+                }
+            }
+        }
+
+        public Countries Country { get; set; }
+
+        public Genders Gender { get; set; }
+
+        public Contacts Contact { get; set; }
+
+
+
+        public int EmployeeAge ()
+        {
+            int age =  DateTime.Now.Year - DateOfBirth.Year;
+
+            // თუ წელს დაბადების დღე არ ჰქონია, მაშინ 1 წელი გამოაკლოს
+            if (DateTime.Now.Month<DateOfBirth.Month || (DateTime.Now.Month==DateOfBirth.Month && DateTime.Now.Day<DateOfBirth.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+
+
+        // ცალკე მეთოდი, რომელიც დაბეჭდავს ინფორმაციას. გამოვიყენებ საერთო ქვეყნების ძიების მეთოდში
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Sunrame: {Surname}, Date of birth: {DateOfBirth}, Country: {Country}, Gender: {Gender}, Contact Info: {Contact}.");
+        }
+        
+
+        public static void CommonCountry(Employee[] employees, Countries targetCountry)
+        {
+            foreach (Employee employee in employees)
+            {
+                if (employee.Country==targetCountry)
+                {
+                    employee.DisplayEmployeeInfo(); 
+                }
+            }
+        }
+
+        
+        
+
+
+    }
+}
---
+++ Assignment 9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Assignment_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ეს ინფორმაცია ალბათ მეინში უნდა დარჩეს...?
+            Employee emp1 = new Employee("Mariam", "Beruashvili", new DateTime(2000, 3, 5), Countries.Georgia,Genders.Female, Contacts.Email);
+            Employee emp2 = new Employee("Giorgi", "Bagrationi", new DateTime(1997, 11, 23), Countries.Georgia, Genders.Male, Contacts.Email);
+            Employee emp3 = new Employee("Dimitrios", "Makris", new DateTime(2003, 6, 2), Countries.Greece, Genders.Male, Contacts.Email);
+            Employee emp4 = new Employee("Ioannis", "Vlachos", new DateTime(1992, 10, 18), Countries.Greece, Genders.Male, Contacts.Email);
+            Employee emp5 = new Employee("Sofia", "Gallo", new DateTime(2001, 12, 15), Countries.Italy, Genders.Female, Contacts.Email);
+            Employee emp6 = new Employee("Chiara", "Costa", new DateTime(1997, 10, 24), Countries.Italy, Genders.Female, Contacts.Email);
+            Employee emp7 = new Employee("Lena", "Wagner", new DateTime(2001, 2, 10), Countries.Germany, Genders.Female, Contacts.Email);
+            Employee emp8 = new Employee("Jonas", "Bauer", new DateTime(1999, 10, 12), Countries.Germany, Genders.Male, Contacts.Email);
+
+            Employee[] employees = {emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8};
+
+            Console.WriteLine("Which employee's age would you like to see? (Enter a number 1-8)");
+            bool isValidNum = byte.TryParse(Console.ReadLine(), out byte num);
+
+            if (isValidNum && num >=1 && num <=8)
+            {
+                Console.WriteLine($"Employee's age is {employees[num - 1].EmployeeAge()}");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+
+
+            Console.WriteLine("Enter your target Country: ");
+
+            // გადავპარსოთ შეყვანილი ქვეყანა Countries ტიპში:
+            Countries targetCountry = (Countries)Enum.Parse(typeof(Countries), Console.ReadLine());
+
+            Employee.CommonCountry(employees, targetCountry);
+
+            
+
+        }
+    }
+}
